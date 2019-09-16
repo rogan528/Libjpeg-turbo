d3c33701cbc53664a0bfe2ef2f33def121e04cff
rogan528 <3097940262@qq.com>
2019-09-16 23:08:06 +0800

    初始化代码

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,10 @@
 #include <jni.h>
 #include <string>
+#include <malloc.h>
+#include <android/bitmap.h>
+#include <jpeglib.h>
+
+void write_JPEG_file(uint8_t *temp, int w, int h, jint quarity, const char *path);
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_android_libjpeg_1turbo_MainActivity_stringFromJNI(
@@ -7,4 +12,81 @@ Java_com_android_libjpeg_1turbo_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+void write_JPEG_file(uint8_t *data, int w, int h, jint quarity, const char *path) {
+
+    jpeg_compress_struct jcs;
+    jpeg_error_mgr error_mgr;
+    jcs.err = jpeg_std_error(&error_mgr);
+    jpeg_create_compress(&jcs);
+    FILE *f=fopen(path,"wd");
+    jpeg_stdio_dest(&jcs, f);
+    jcs.image_width = w;
+    jcs.image_height=h;
+
+    jcs.input_components =3;
+    jcs.in_color_space=JCS_RGB;
+    jpeg_set_defaults(&jcs);
+    //开启hash
+    jcs.optimize_coding= true;
+    jpeg_set_quality(&jcs,quarity,1);
+    jpeg_start_compress(&jcs,1);
+    int row_stride=w*3;
+    JSAMPROW row[1];
+    while (jcs.next_scanline<jcs.image_height){
+        uint8_t *pixels=data+jcs.next_scanline*row_stride;
+        row[0]=pixels;
+        jpeg_write_scanlines(&jcs,row,1);
+
+
+    }
+    jpeg_finish_compress(&jcs);
+    fclose(f);
+    jpeg_destroy_compress(&jcs);
+
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_android_libjpeg_1turbo_MainActivity_nativeCompress(JNIEnv *env, jobject instance,
+                                                            jobject bitmap, jint quarity,
+                                                            jstring path_) {
+    const char *path = env->GetStringUTFChars(path_, 0);
+
+    // TODO
+    //获取argb数据
+    AndroidBitmapInfo bitmapInfo;
+    //获取里面的信息
+    AndroidBitmap_getInfo(env,bitmap,&bitmapInfo);
+
+    uint8_t *pixels;
+    AndroidBitmap_lockPixels(env,bitmap,(void **)&pixels);
+
+    int w = bitmapInfo.width;
+    int h = bitmapInfo.height;
+    int color;
+    uint8_t r,g,b;
+    uint8_t *data=(uint8_t *)malloc(w*h*3);
+    uint8_t *temp = data;
+    for (int i=0;i<h;i++){
+        for (int j=0;j<w;j++){
+            color=*(int *)pixels;
+            r=(color>>16) & 0xFF;
+            g=(color>>8) & 0xFF;
+            b=color & 0xFF;
+            *data=b;
+            *(data+1)=g;
+            *(data+2)=r;
+            data+=3;
+            pixels +=4;
+        }
+
+    }
+    write_JPEG_file(temp,w,h,quarity,path);
+
+    free(data);
+    AndroidBitmap_unlockPixels(env,bitmap);
+    env->ReleaseStringUTFChars(path_, path);
 }
--- app/src/main/java/com/android/libjpeg_turbo/MainActivity.java
+++ app/src/main/java/com/android/libjpeg_turbo/MainActivity.java
@@ -2,10 +2,20 @@ package com.android.libjpeg_turbo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    Bitmap bitmap;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -20,11 +30,58 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        //bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.timg);
+
+       /* compress(bitmap,Bitmap.CompressFormat.JPEG,50,
+                Environment.getExternalStorageDirectory()
+                +"/test_q.jpeg");
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap,300,300,true);
+        compress(scaledBitmap,Bitmap.CompressFormat.JPEG,100,
+                Environment.getExternalStorageDirectory() +"/test_scaled.jpeg");
+
+        compress(bitmap,Bitmap.CompressFormat.PNG,100,
+                Environment.getExternalStorageDirectory()+"/test.png");
+        (bitmap,Bitmap.CompressFormat.WEBP,100,
+                Environment.getExternalStorageDirectory()+"/test.wep");*/
+        File input = new File(Environment.getExternalStorageDirectory(),"test.jpg");
+        bitmap= BitmapFactory.decodeFile(input.getAbsolutePath());
+
+
     }
 
+    /**
+     * @param bitmap 待压缩图片
+     * @param compressFormat 压缩格式
+     * @param quarity 质量
+     * @param path 文件地址
+     */
+    private void compress(Bitmap bitmap, Bitmap.CompressFormat compressFormat,int quarity,String path){
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(path);
+            bitmap.compress(compressFormat,quarity,fileOutputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if (null != fileOutputStream){
+                try {
+                    fileOutputStream.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native void nativeCompress(Bitmap bitmap,int quarity,String path);
+
+    public void test(View view) {
+        nativeCompress(bitmap,50,Environment.getExternalStorageDirectory()+"/timg22.jpg");
+    }
 }
